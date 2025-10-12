065975117faf845af1b305744938c73180a68c64
Brandon <wififished@gmail.com>
2025-10-12 02:50:38 -0700

    Update script.js

--- script.js
+++ script.js
@@ -85,7 +85,7 @@ document.addEventListener('DOMContentLoaded', function() {
             } else if (window.innerWidth <= 992) {
                 slidesPerPage = 2;
             } else {
-                slidesPerPage = 3;
+                slidesPerPage = 4;
             }
         };
 
