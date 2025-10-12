654b824f0337d6e38ef21e2c7b766b5ac6438f8e
Brandon <wififished@gmail.com>
2025-10-11 20:03:34 -0700

    Update script.js

--- script.js
+++ script.js
@@ -62,3 +62,41 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 
 });
+
+// --- Gallery Carousel ---
+const track = document.querySelector('.carousel-track');
+const slides = Array.from(track.children);
+const nextButton = document.querySelector('.carousel-button.next');
+const prevButton = document.querySelector('.carousel-button.prev');
+
+if (track) {
+    const slideWidth = slides[0].getBoundingClientRect().width;
+
+    // Arrange the slides next to one another
+    const setSlidePosition = (slide, index) => {
+        slide.style.left = slideWidth * index + 'px';
+    };
+    slides.forEach(setSlidePosition);
+
+    const moveToSlide = (track, currentSlide, targetSlide) => {
+        track.style.transform = 'translateX(-' + targetSlide.style.left + ')';
+        currentSlide.classList.remove('current-slide');
+        targetSlide.classList.add('current-slide');
+    }
+
+    // When I click left, move slides to the left
+    prevButton.addEventListener('click', e => {
+        const currentSlide = track.querySelector('.current-slide') || slides[0];
+        const prevSlide = currentSlide.previousElementSibling || slides[slides.length - 1];
+
+        moveToSlide(track, currentSlide, prevSlide);
+    });
+
+    // When I click right, move slides to the right
+    nextButton.addEventListener('click', e => {
+        const currentSlide = track.querySelector('.current-slide') || slides[0];
+        const nextSlide = currentSlide.nextElementSibling || slides[0];
+
+        moveToSlide(track, currentSlide, nextSlide);
+    });
+}
